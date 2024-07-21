65aedf3089c2d9e35ea255f5a017ce69495edfa6
Vishesh Singh <VisheshSingh16@gmail.com>
2024-07-21 08:29:56 -0400

    Loader and error components

---
+++ src/components/ErrorMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorMessage = () => {
+  return (
+    <div>
+      <h3>Something went wrong</h3>
+      <p>⛔</p>
+    </div>
+  );
+};
+
+export default ErrorMessage;
